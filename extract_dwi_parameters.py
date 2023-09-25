651f8e2e1dbace3b9c624105569d906449f7e58e
shoyagishita <yagishita-tky@umin.ac.jp>
2023-09-25 13:43:19 +0900

    Update extract_dwi_parameters.py

--- extract_dwi_parameters.py
+++ extract_dwi_parameters.py
@@ -6,13 +6,29 @@ import os
 KEY1="##$PVM_DwGradVec="
 KEY2="##$PVM_DwEffBval="
 
+'''
+Data structure of the target file
+##$PVM_DwDir=( 112, 3 )
+0.129705582985459 0.172861745083561 0.976368413473658 -0.482657029520601 
+0.281067646465789 0.829483676731816 -0.187812786427214 -0.375659578124733 
+0.907527541520142 0.514580221281473 -0.309726986323259 0.79954761572343 
+0.652087947068601 0.372677764036738 0.660221624517431 0.0414513540615393 
+0.735256725113697 0.676520017014683 -0.743955379444463 -0.260941271375216 
+0.615174809536876 -0.455570292140411 -0.79494401224997 0.400649131169682
+'''
 def extract_mat(method_text,key):
+    '''
+    Extracting data following the {key} until it finds next key by ##
+    '''
     subm=method_text[method_text.find(key)+len(key):]
     subm=subm[:subm.find("##")]
     subm=subm.replace(" ","\n").replace("\n\n","\n")
     return subm
 
 def str2array(mat):
+    '''
+    converting str numbers to numpy array
+    '''
     mat=map(lambda x:float(x),filter(lambda x:len(x)>0,mat.split("\n")))
     return np.array(list(mat))
 
@@ -22,6 +38,7 @@ def create_b_table(m_file):
         method_text = f.readlines()
         method_text="\n".join(method_text)
 
+    # extracting b vectors
     param,zmat,mat=extract_mat(method_text,KEY1).split(")")
     mat=str2array(mat)
     pattern = r'@(\d+)\*\(0'
@@ -30,6 +47,7 @@ def create_b_table(m_file):
     mat=np.concatenate([np.zeros(int(match.group(1))),mat])
     d=pd.DataFrame(mat.reshape([-1,3]),columns=["bx","by","bz"])
 
+    # extracting b values
     param,mat=extract_mat(method_text,KEY2).split(")")
     mat=str2array(mat)
     d["b"]=mat
