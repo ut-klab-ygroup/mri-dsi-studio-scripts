c5ebc9d5f8e1c5f1f317e4ebeabcc396efd5540e
AnriiGegliuk <andrii.gegliuk@gmail.com>
2024-05-02 12:19:19 +0900

    added create_dsi_database function to full pipline

--- main_logic.py
+++ main_logic.py
@@ -1,7 +1,8 @@
 import os
 from modalities.extract_raw_data_bruker import create_and_process_dti_dirs
-from modalities.utilities.utils import list_projects, select_project, is_directory_empty
+from modalities.utilities.utils import list_projects, select_project
 from modalities.reconstruction import process_src_files
+from modalities.data_base_creation import create_dsi_database
 
 
 if __name__ == "__main__":
@@ -10,14 +11,18 @@ if __name__ == "__main__":
 
     # function listing all directories within input folder (input folder expected to have only raw data under the specified project name)
     projects = list_projects(input_dir)
+
     if projects:
         # processing data based on the project that was selected
         selected_project = select_project(projects)
+
         if selected_project:
             project_input_dir = os.path.join(input_dir, selected_project)
+
             if not any(os.listdir(project_input_dir)):
                 print(f"The project directory '{selected_project}' is empty. Please add raw data from Paravision 360 to perform analysis.")
             else:
+
                 project_output_dir = os.path.join(output_dir, selected_project, 'DTI', 'preprocessing')
                 os.makedirs(project_output_dir, exist_ok=True)
                 print(f"Processing data for project {selected_project}...")
@@ -29,6 +34,10 @@ if __name__ == "__main__":
                 print("Starting the reconstruction of .src.gz files...")
                 reconstruction_message = process_src_files(project_output_dir)
                 print(reconstruction_message)
+
+                # STEP C1: Creating database after 
+                print("Starting the creation of the DSI Studio database...")
+                create_dsi_database(project_output_dir)
                 
                 print("Processing completed.")
         else:
