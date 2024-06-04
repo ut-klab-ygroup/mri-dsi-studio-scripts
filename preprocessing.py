6a42450f54178f3151f66260b0510059a259a79a
Andrii Gegliuk <andrii.gegliuk@gmail.com>
2024-06-04 14:08:53 +0900

    message to user style changes

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 import os
 import csv
-from colorama import init, Fore, Style
+from colorama import init, Fore, Style, Back
 
 from modalities.extract_raw_data_bruker import create_and_process_dti_dirs
 from modalities.utilities.utils import list_projects, select_project
@@ -29,21 +29,21 @@ if __name__ == "__main__":
 
                 project_output_dir = os.path.join(output_dir, selected_project, 'DTI', 'preprocessing')
                 os.makedirs(project_output_dir, exist_ok=True)
-                print(Fore.GREEN+ Style.BRIGHT + f"Processing data for project {selected_project}..." + Style.RESET_ALL )
+                print(Back.GREEN + f"Processing data for project {selected_project}..." + Style.RESET_ALL )
                 
                 # STEP T1: Data extraction and preparation
                 create_and_process_dti_dirs(project_input_dir, project_output_dir)
                 
                 # STEP T2: Reconstruction process
-                print(Fore.GREEN + Style.BRIGHT + "Starting the reconstruction of .src.gz files..." + Style.RESET_ALL)
+                print(Back.GREEN + "Starting the reconstruction of .src.gz files..." + Style.RESET_ALL)
                 reconstruction_message = process_src_files(project_output_dir)
                 print(reconstruction_message)
 
                 # STEP C1: Creating database (normalization & registration for group-level analysis) 
-                print(Fore.GREEN + Style.BRIGHT + "Starting the creation of the DSI Studio database..." + Style.RESET_ALL)
+                print(Back.GREEN + "Starting the creation of the DSI Studio database..." + Style.RESET_ALL)
                 create_dsi_database(project_output_dir)
                 
-                print(Fore.GREEN + Style.BRIGHT + "Processing completed. Please open database in DSI Studio to check if there is any artifacts or missregistration to remove some subjects!" + Style.RESET_ALL)
+                print(Fore.GREEN + "Processing completed. Please open database in DSI Studio to check if there is any artifacts or missregistration to remove some subjects!" + Style.RESET_ALL)
         else:
             print(Fore.RED + Style.BRIGHT + "No valid project selected, exiting." + Style.RESET_ALL)
     else:
