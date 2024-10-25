fd0ec7a10ac1a43d9093bc7e72dc619954f204c5
Andrii Gegliuk <andrii.gegliuk@gmail.com>
2024-10-25 20:37:20 +0900

    modified pipline and looks better

--- new_preprocessing_pipline.py
+++ new_preprocessing_pipline.py
@@ -12,48 +12,17 @@ from modalities.utilities.utils import (
     group_by_condition,
     get_subject_name
 )
+
+from modalities.utilities.extraction_configuration import (
+    load_configuration,
+    save_configuration,
+    update_configuration,
+    ask_reconstruction
+)
+
 from modalities.reconstruction import process_src_files
-from modalities.data_base_creation import create_dsi_database
 
 
-def load_configuration(config_file):
-    """Load the configuration file (JSON) or create a new one if it doesn't exist."""
-    if os.path.exists(config_file):
-        with open(config_file, 'r') as f:
-            return json.load(f)
-    return {"projects": {}}
-
-# save database based on extracted files
-def save_configuration(config, config_file):
-    with open(config_file, 'w') as f:
-        json.dump(config, f, indent=4)
-
-
-def update_configuration(config, project_name, subject_name, cohort):
-    if project_name not in config['projects']:
-        config['projects'][project_name] = {"subjects_processed": {}, "cohorts": {}, "last_run": ""}
-
-    config['projects'][project_name]['subjects_processed'][subject_name] = "processed"
-
-    if cohort not in config['projects'][project_name]['cohorts']:
-        config['projects'][project_name]['cohorts'][cohort] = []
-
-    if subject_name not in config['projects'][project_name]['cohorts'][cohort]:
-        config['projects'][project_name]['cohorts'][cohort].append(subject_name)
-
-    # capturing date
-    config['projects'][project_name]['last_run'] = datetime.now().strftime("%Y-%m-%d")
-
-def ask_reconstruction():
-    """Ask the user if they want to rerun reconstruction for all previously processed subjects."""
-    while True:
-        user_choice = input("Do you want to rerun the reconstruction for existing subjects? (yes/no): ").strip().lower()
-        if user_choice in ['yes', 'y']:
-            return True
-        elif user_choice in ['no', 'n']:
-            return False
-        else:
-            print("Please enter 'yes' or 'no'.")
 
 if __name__ == "__main__":
     input_dir = r"C:\preprocessing\raw_analysis"
@@ -107,7 +76,7 @@ if __name__ == "__main__":
                                 subject_name = get_subject_name(subject_id)
                                 subject_preprocessing_dir = os.path.join(condition_output_dir, subject_name)
 
-                                # Checking if the reconstructed file already exists
+                                # checking if the reconstructed file already exists
                                 reconstructed_file = os.path.join(subject_preprocessing_dir, f"{subject_name}.src.gz.dti.fib.gz")
                                 if os.path.exists(reconstructed_file):
                                     existing_reconstructions.append(subject_name)
@@ -160,7 +129,7 @@ if __name__ == "__main__":
                                 # Ensure subject_name matches subject_name_extracted
 
                                 if subject_preprocessing_dir and subject_name_extracted:
-                                    # STEP T2: Reconstruction process
+                                    # STEP T2: Reconstruction process based on [Step T2][Edit][Image flip z] (fix when proper orientation fixed)
                                     print(Fore.YELLOW + "Starting reconstruction process for .src.gz files..." + Style.RESET_ALL)
                                     process_src_files(subject_preprocessing_dir)
                                     print(Fore.GREEN + Style.BRIGHT + f"Processed files for subject '{subject_name_extracted}'." + Style.RESET_ALL)
